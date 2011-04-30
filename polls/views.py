5fc84cf112bbaca09ac4c7431e74373d5c404a27
Haihong Xu <xu_haha@yahoo.com>
2011-04-30 14:48:53 -0700

    update templates

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,29 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from polls.models import Poll
+
 def redirect_to_polls(request):
     return HttpResponseRedirect('/polls/')
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    #return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    #output = ', '.join([p.question for p in latest_poll_list])
+    #return HttpResponse(output)
+    context = {'latest_poll_list': latest_poll_list}
+    return render_to_response('index.html', context)
+
+from django.http import Http404
 
 def detail(request, poll_id):
-    return HttpResponse("You are looking at poll %s." % (poll_id,))
+    #return HttpResponse("You are looking at poll %s." % (poll_id,))
+    try:
+        p = Poll.objects.get(id=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render_to_response('polls/detail.html', {'poll': p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll <strong>%s</strong>." % (poll_id,))
