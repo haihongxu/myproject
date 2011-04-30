b412fa03c0353f56db964aaf5fbcefd3f121652a
Haihong Xu <xu_haha@yahoo.com>
2011-04-30 13:40:47 -0700

    update urls and views

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,18 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+def redirect_to_polls(request):
+    return HttpResponseRedirect('/polls/')
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the poll index.")
+
+def detail(request, poll_id):
+    return HttpResponse("You are looking at poll %s." % (poll_id,))
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll <strong>%s</strong>." % (poll_id,))
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % (poll_id,))
+
